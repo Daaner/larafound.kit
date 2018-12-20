e1e2e6ad9f7d8773ddc0d89978a1d16e082ce37f
Daan <daan@ukr.net>
2018-12-20 23:37:30 +0200

    package assets

--- resources/lang/en/site.php
+++ resources/lang/en/site.php
@@ -9,15 +9,15 @@ return [
     'register' => 'Register',
 	'logout' => 'Logout',
 	'adm_panel' => 'Admin panel',
-	
+
 	'register_header' => 'Registration',
 	'name' => 'Name',
 	'email' => 'E-mail',
 	'password' => 'Password',
 	'password_again' => 'Re-enter password',
-	
+
 	'form_password_re_enter_error' => 'Passwords are supposed to match',
 	'form_error' => 'There are some errors in your form.',
-	
+
 
 ];
